12a99e9a39928eaf62ae765c8e1ae6eadae08e56
Thomas Lin <thomaslin@ThomasdeMacBook-Pro-338.local>
2025-05-14 15:16:19 -0500

    Update object_pose_detection.py and add parallel_jaw_gripper.py and image8.png

--- parallel_jaw_gripper.py
+++ parallel_jaw_gripper.py
@@ -26,7 +26,7 @@ def display_image(img):
 if __name__ == "__main__":
 
     # Image filename
-    image_name = 'image7.png'  # Change this to an image filename in this directory
+    image_name = 'image2.png'  # Change this to an image filename in this directory
     img = detect_image(image_name)
 
     # Convert the image to grayscale
@@ -58,13 +58,35 @@ if __name__ == "__main__":
     # Visualize the center point
     cv2.circle(img, tuple(map(int, center)), 5, (255, 0, 0), -1)  # blue dot at center
 
+    # Find lengths of two principal axes, from center of object to its contour
+    principal_axes_lengths = []
+    for axis in principal_axes:
+        projections = np.dot(points - center, axis)  # project points onto the principal axis
+            # points - center: shift all points to the origin (0,0)
+            # np.dot: by using dot product between points and principal axis (unit vector), we get the projection of points onto the principal axis
+            # projections: an array of distances from the center to the points along the principal axis that the points are projected onto
+        min_proj = np.min(projections)
+        max_proj = np.max(projections)
+        half_length = (max_proj - min_proj) / 2 # divide the difference of max and min projections by 2 to get distance from object center to its contour
+        principal_axes_lengths.append(half_length)
+    
     # Visualize principal axes
     colors = [(0, 0, 255), (0, 255, 0)] # color red for the first axis (long) and green for the second axis (short)
     for i, axis in enumerate(principal_axes):
-        length = int(eigenvalues[i] * 0.2) # scale eigenvalue for visibility
-        end_point = center + axis * length
+        # length = int(eigenvalues[i] * 0.2) # scale eigenvalue for visibility
+        end_point = center + axis * principal_axes_lengths[i]
         end_point = tuple(map(int, end_point))
         cv2.line(img, tuple(map(int, center)), end_point, colors[i], 2) # image / start point / end point / color / thickness
+
+    # Visualize the parallel jaw gripper, draw two parallel lines along the second eigenvector
+    gripper_offset = 30 # distance from object contour to the gripper along the second principal axis (short)
+    half_gripper_length = 100
+    for sign in [-1, 1]: # sign: for drawing two parallel lines on both sides of the object
+        start_point = center + sign * principal_axes[1] * (principal_axes_lengths[1] + gripper_offset) + principal_axes[0] * half_gripper_length
+        end_point = center + sign * principal_axes[1] * (principal_axes_lengths[1] + gripper_offset) - principal_axes[0] * half_gripper_length
+        start_point = tuple(map(int, start_point))
+        end_point = tuple(map(int, end_point))
+        cv2.line(img, start_point, end_point, (0, 165, 255), 2)
 
     # Display the image
     display_image(img)
